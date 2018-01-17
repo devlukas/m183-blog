c307c8383c21aebaeaa17a208800c1f97e04415b
Pascal Aregger <pascal.aregger97@gmail.com>
2018-01-17 22:32:28 +0100

    Add admin dashboard

---
+++ M183.Blog/Controllers/AdminDashboardController.cs
@@ -0,0 +1,18 @@
+﻿using M183.Blog.Manager;
+using M183.Blog.Models;
+using System.Web.Mvc;
+
+namespace M183.Blog.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        // GET: AdminDashboard
+        public ActionResult Index()
+        {
+            return View(new DashboardViewModel()
+            {
+                Posts = new PostManager().GetPosts()
+            });
+        }
+    }
+}
